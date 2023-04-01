1999414572f4e4ba8c582fcd32bcf299ad6298f9
saipratap9 <skondreddy@presidio.com>
2023-04-01 11:34:59 +0530

    Changed the deployed backend url

--- src/app/modules/shared/models/urls.model.ts
+++ src/app/modules/shared/models/urls.model.ts
@@ -1,3 +1,3 @@
-let globalVars = {backendAPI: 'http://localhost:8080/api/v1' };
+let globalVars = {backendAPI: 'http://ec2-54-90-49-175.compute-1.amazonaws.com:8080/api/v1' };
 
 export { globalVars };
